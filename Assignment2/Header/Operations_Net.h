28e62ef3e0a3d5842e77fb184e078136adb75459
Julian <jbostick1@email.arizona.edu>
2015-09-27 18:51:26 -0700

    Operation class header
    adding header for operation class

---
+++ Assignment2/Header/Operations_Net.h
@@ -0,0 +1,15 @@
+class Operation_Net{
+private:
+    std::string a;
+    std::string b;
+    std::string clk;
+    std::string rst;
+    std::string sel;
+    std::string o1;
+    std::string o2;
+    std::string o3;
+    datpathComp component;
+    int size;
+    bool sign;
+    std::string instantiation;
+}
